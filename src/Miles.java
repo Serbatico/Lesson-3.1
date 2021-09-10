61afd39d7753d035c1d5a9fb37b4fcc8e8406031
Serbatico <Serbatico@yandex.ru>
2021-09-10 16:27:06 +0300

    -a

---
+++ src/Miles.java
@@ -0,0 +1,13 @@
+public class Miles {
+
+    public static void main(String[] args) {
+
+        int ticketPrice = 12342; //Цена билета
+
+
+        byte moneyPerMiles = 20; //Отвечая вопросом на Ваш вопрос:"А почему нет ?")
+        int bonusMiles = ticketPrice / moneyPerMiles;
+        System.out.println(bonusMiles);
+
+    }
+}
