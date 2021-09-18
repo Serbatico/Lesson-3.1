0015443b6c4ef4ba27ac4f23f681139675e6b7c9
Anton Kirichenko <86833434+Serbatico@users.noreply.github.com>
2021-09-18 15:28:53 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,6 @@ public class BonusMilesService {
 
     public static int calculate (int cost) {
 
-//        int cost = 10000;
         byte moneyPerMiles = 20;
         int bonusMiles = cost / moneyPerMiles;
         return bonusMiles ;
